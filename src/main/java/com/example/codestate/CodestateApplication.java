ba3bc89b5e80a7eef009a6001ea02ac114d03016
Sundeep Malik <sundeepmalik007@gmail.com>
2019-08-11 22:54:28 -0400

    initial commit

---
+++ src/main/java/com/example/codestate/CodestateApplication.java
@@ -0,0 +1,13 @@
+package com.example.codestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodestateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodestateApplication.class, args);
+    }
+
+}
